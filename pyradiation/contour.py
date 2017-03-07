265b2092d60e64a010dea523764d513e45778b84
Martin Landa <landa.martin@gmail.com>
2017-03-07 14:42:03 +0100

    RadiationIsolines: rename class variables, clean-up code

--- pyradiation/contour.py
+++ pyradiation/contour.py
@@ -4,27 +4,35 @@ from .exception import RadiationError
 
 class RadiationIsolines:
     def __init__(self, raster):
-        self.ds = gdal.Open(raster)
-        if self.ds is None:
+        self.input_ds = gdal.Open(raster)
+        if self.input_ds is None:
             raise RadiationError("Unable to open '{}'".format(raster))
 
-        print("Number of bands: {}".format(self.ds.RasterCount))
+        self.output_ds = None
+        # TODO: logging
+        # print("Number of bands: {}".format(self.input_ds.RasterCount))
 
     def __del__(self):
-        self.ds = None
+        self.input_ds = None
+        if self.output_ds is not None:
+            self.output_ds.Destroy()
 
     def destination(self, dst_filename):
         # Generate layer to save isolines in
-        self.ogr_ds = ogr.GetDriverByName("ESRI Shapefile").CreateDataSource(dst_filename)
-        self.contour_shp = self.ogr_ds.CreateLayer('contour')
+        self.output_ds = ogr.GetDriverByName("ESRI Shapefile").CreateDataSource(dst_filename)
+        self.output_layer = self.output_ds.CreateLayer('isolines')
 
-        self.field_defn = ogr.FieldDefn("ID", ogr.OFTInteger)
-        self.contour_shp.CreateField(self.field_defn)
-        self.field_defn = ogr.FieldDefn("elev", ogr.OFTReal)
-        self.contour_shp.CreateField(self.field_defn)
+        field_defn = ogr.FieldDefn("ID", ogr.OFTInteger)
+        self.output_layer.CreateField(field_defn)
+        field_defn = ogr.FieldDefn("elev", ogr.OFTReal)
+        self.output_layer.CreateField(field_defn)
 
     def generate(self, levels):
         # Generate isolines with fixed levels
         print("Level count: {}".format(levels))
-        self.band = self.ds.GetRasterBand(1)
-        gdal.ContourGenerate(self.band, 0, 0, levels, 0, 0, self.contour_shp, 0, 1)
+        if self.output_ds is None:
+            raise RadiationError("Output datasource not defined, destination() method must be called.")
+        band = self.input_ds.GetRasterBand(1)
+        ret = gdal.ContourGenerate(band, 0, 0, levels, 0, 0, self.output_layer, 0, 1)
+        if ret != gdal.CE_None:
+            raise RadiationError("Isolines generation failed")
