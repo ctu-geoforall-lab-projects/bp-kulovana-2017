84aab63e83f86c716e295aff7ebb013f8d7d77bb
Martin Landa <landa.martin@gmail.com>
2017-03-14 14:52:52 +0100

    improve polygonizer skeleton (see #2)

--- pyradiation/polygonizer.py
+++ pyradiation/polygonizer.py
@@ -8,5 +8,32 @@ class RadiationPolygonizer:
     def destination(self, dst_filename=None, overwrite=True):
         pass
 
+    def _close_lines(self):
+        # self.input_lines.layer()
+        # [ (geom1, value1), (geom2, value2), ...]
+        return []
+
+    def _create_polygons(self, geoms):
+        # [ (geom1, value1), (geom2, value2), ...]
+        return []
+
+    def _write_output(self):
+        # check if destination is defined
+        pass
+
     def polygonize(self):
+        # 1. close open isolines (based on bbox)
+        # print warning if isolines cannot be closed
+        lines = self._close_lines()
+
+        # 2. perform generalization if defined
+        if generalizer:
+            lines = generalizer.perform(lines)
+
+        # 3. create polygons from closed lines
+        polygons = self._polygonize(lines)
+
+        # 4. write polygons to output
+        self._write_output()
+
         print (self.input_lines.layer().GetFeatureCount())
