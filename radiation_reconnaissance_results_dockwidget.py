a86ca60a16d925e381cf6483955a5c42f366cc71
pikorcik <tkule@seznam.cz>
2017-05-08 22:47:16 +0200

    Connect dockwidget with library. Get isolines levels from UI checkboxes.

--- radiation_reconnaissance_results_dockwidget.py
+++ radiation_reconnaissance_results_dockwidget.py
@@ -60,9 +60,24 @@ class RadiationReconnaissanceResultsDockWidget(QtGui.QDockWidget, FORM_CLASS):
         # Set filter for QgsMapLayerComboBox
         self.raster_box.setFilters(QgsMapLayerProxyModel.RasterLayer)
 
+        self.doserate_label.setText("Activity [MBq/m^2]")
+
         self.load_raster.clicked.connect(self.onLoadRaster)
-        self.solve_button.setEnabled(False)
+        self.solve_button.setEnabled(True)
+        self.solve_button.clicked.connect(self.onSaveButton)
         self.report_button.clicked.connect(self.onReportButton)
+        self.type_box.currentIndexChanged.connect(self.onTypeBox)
+
+        # Level checkboxes (Activity checkboxes checked, others disabled)
+        self.onTypeBox()
+        self.check_001.stateChanged.connect(self.levelsUpdate)
+        self.check_01.stateChanged.connect(self.levelsUpdate)
+        self.check_1.stateChanged.connect(self.levelsUpdate)
+        self.check_5.stateChanged.connect(self.levelsUpdate)
+        self.check_10.stateChanged.connect(self.levelsUpdate)
+        self.check_50.stateChanged.connect(self.levelsUpdate)
+        self.check_100.stateChanged.connect(self.levelsUpdate)
+        self.check_1000.stateChanged.connect(self.levelsUpdate)
 
     def closeEvent(self, event):
         self.closingPlugin.emit()
@@ -82,3 +97,116 @@ class RadiationReconnaissanceResultsDockWidget(QtGui.QDockWidget, FORM_CLASS):
 
     def onReportButton(self):
         pass
+
+    def onTypeBox(self):
+        # Activity = 0, Doserate = 1
+        print "zmena indexu"
+        index = self.type_box.currentIndex()
+        print index
+        if index == 0:
+            print "Activity chosen"
+            self.doserate_label.setText("Activity [MBq/m^2]")
+            # Preset levels (rest disabled)
+            self.check_001.setEnabled(True)
+            self.check_001.setChecked(True)
+            self.check_01.setEnabled(False)
+            self.check_01.setChecked(False)
+            self.check_1.setEnabled(True)
+            self.check_1.setChecked(True)
+            self.check_5.setEnabled(False)
+            self.check_5.setChecked(False)
+            self.check_10.setEnabled(True)
+            self.check_10.setChecked(True)
+            self.check_50.setEnabled(False)
+            self.check_50.setChecked(False)
+            self.check_100.setEnabled(True)
+            self.check_100.setChecked(True)
+            self.check_1000.setEnabled(False)
+            self.check_1000.setChecked(False)
+
+        elif index == 1:
+            print "Doserate chosen"
+            self.doserate_label.setText("Doserate [cGy/h]")
+            # Preset levels (rest disabled)
+            self.check_001.setEnabled(False)
+            self.check_001.setChecked(False)
+            self.check_01.setEnabled(True)
+            self.check_01.setChecked(True)
+            self.check_1.setEnabled(True)
+            self.check_1.setChecked(True)
+            self.check_5.setEnabled(True)
+            self.check_5.setChecked(True)
+            self.check_10.setEnabled(True)
+            self.check_10.setChecked(True)
+            self.check_50.setEnabled(True)
+            self.check_50.setChecked(True)
+            self.check_100.setEnabled(True)
+            self.check_100.setChecked(True)
+            self.check_1000.setEnabled(True)
+            self.check_1000.setChecked(True)
+
+        else:
+            print "Problem with choosing type of data"
+
+    def levelsUpdate(self):
+        print "Checkbox clicked"
+        self.levels = []
+        if self.check_001.isChecked():
+            self.levels.append(0.01)
+        if self.check_01.isChecked():
+            self.levels.append(0.1)
+        if self.check_1.isChecked():
+            self.levels.append(1)
+        if self.check_5.isChecked():
+            self.levels.append(5)
+        if self.check_10.isChecked():
+            self.levels.append(10)
+        if self.check_50.isChecked():
+            self.levels.append(50)
+        if self.check_100.isChecked():
+            self.levels.append(100)
+        if self.check_1000.isChecked():
+            self.levels.append(1000)
+
+    def onSaveButton(self):
+        print "Save button clicked"
+        print "levely"
+        self.levelsUpdate()
+        print self.levels
+
+        raster = self.raster_box.currentLayer().source()
+        print "cesta k rastru"
+        print type(raster)
+        print type(raster.encode('ascii', 'ignore'))
+        raster = raster.encode('ascii', 'ignore')
+        raster = raster.split("/")
+        print raster
+        raster = "\\".join(raster)
+        print type(raster)
+
+        text = "cesta/k/rastru"
+        print type(text)
+        text = text.split("/")
+        print text
+        text = "\\".join(text)
+        print type(text)
+
+        rc = isolines.RadiationIsolines(raster)
+        output_dir = os.path.dirname(raster)
+        output_filename = '{}_isolines.shp'.format(
+            os.path.splitext(os.path.basename(raster))[0]
+            )
+        output = os.path.join(output_dir, output_filename)
+        rc.destination(output)
+        rc.generate(self.levels)
+        print('{} generated'.format(output))
+
+        rp = polygonizer.RadiationPolygonizer(rc)
+        output_filename = '{}_polygons.shp'.format(
+            os.path.splitext(os.path.basename(raster))[0]
+        )
+        output = os.path.join(output_dir, output_filename)
+        print output
+        rp.destination(output)
+        rp.polygonize()
+        print('{} generated'.format(output))
